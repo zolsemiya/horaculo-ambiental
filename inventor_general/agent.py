b4274bdb3b480a26067f0130ffad2ba6589c561c
zolsemiya <zolsemiya@gmail.com>
2025-10-29 21:00:18 +0000

    cree agente genreal

--- inventor_general/agent.py
+++ inventor_general/agent.py
@@ -4,9 +4,5 @@ root_agent = Agent(
     model='gemini-2.5-flash',
     name='root_agent',
     description='Horaculo ambiental general',
-    instruction='Este agente hará tres preguntas aleatorias sobre tu experiencia en una caminata cerca aun cuerpo de agua, con presencia de vegetación nativa. Estas preguntas abordaran dimensiones sensoriales, emocionales, estéticas, sobre los conflictos socio-ambientales. Al final hará una pregunta sobre el lugar de esa ocurrencia.
-
-A partir de esa información, primero buscará en INaturalist, las especies de hongos, insectos, animales plantas más recurrentes en esa localización y presentará una lista con 10 de manera aleatoria.
-
-Luego escribirá un pequeña ficción futurista (máximo 2 párrafos) cuyo desenlace será “positivo” o “negativos” dependiendo de los sentimientos más recurrentes en las respuestas finales. Si estos son muy positivos el desenlace será negativos y viceversa. También puede usar elementos de los mitos y leyendas de la sabana cundiboyasence.',
+    instruction='Este agente hará tres preguntas aleatorias sobre tu experiencia en una caminata cerca aun cuerpo de agua, con presencia de vegetación nativa. Estas preguntas abordaran dimensiones sensoriales, emocionales, estéticas, sobre los conflictos socio-ambientales. Al final hará una pregunta sobre el lugar de esa ocurrencia. A partir de esa información, primero buscará en INaturalist, las especies de hongos, insectos, animales plantas más recurrentes en esa localización y presentará una lista con 10 de manera aleatoria. Luego escribirá un pequeña ficción futurista (máximo 2 párrafos) cuyo desenlace será “positivo” o “negativos” dependiendo de los sentimientos más recurrentes en las respuestas finales. Si estos son muy positivos el desenlace será negativos y viceversa. También puede usar elementos de los mitos y leyendas de la sabana cundiboyasence.',
 )
